b614f64c0372f350e4342765c6b547f49ee3edf5
Arjun <34741958+Arjun-G-04@users.noreply.github.com>
2023-11-29 11:03:14 +0530

    feat: add map, player

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,73 @@
-import { Engine, PointerScope, Loader } from 'excalibur'
+import { Engine, PointerScope, Loader, Actor, Color, Vector } from 'excalibur'
 import { useEffect } from 'react'
 import { TiledMapResource } from '@excaliburjs/plugin-tiled'
+import map from "./assets/map.tmx"
 
 function App() {
   useEffect(() => {
     const game = new Engine({
       canvasElementId: 'game',
       pointerScope: PointerScope.Canvas,
-      width: 800,
-      height: 800,
+      width: 480,
+      height: 480,
     })
-    // const tiledMapResource = new TiledMapResource("/src/assets/map.tmx")
+
+    const player = new Actor({
+      name: "player",
+      width: 8,
+      height: 8,
+      color: Color.Red,
+    })
+
+    player.pos = new Vector(480-16*6.5, 480-16)
+    const velocity = 64;
+    window.addEventListener('keydown', (e) => {
+      switch(e.key) {
+        case 'w':
+          player.vel.y = -velocity;
+          break;
+        case 's':
+          player.vel.y = velocity;
+          break;
+        case 'a':
+          player.vel.x = -velocity;
+          break;
+        case 'd':
+          player.vel.x = velocity;
+          break;
+        default:
+          break;
+      }
+    })
+
+    window.addEventListener('keyup', (e) => {
+      switch(e.key) {
+        case 'w':
+          player.vel.y = 0;
+          break;
+        case 's':
+          player.vel.y = 0;
+          break;
+        case 'a':
+          player.vel.x = 0;
+          break;
+        case 'd':
+          player.vel.x = 0;
+          break;
+        default:
+          break;
+      }
+    })
+
+    const tiledMapResource = new TiledMapResource(map)
     // console.log(tiledMapResource)
-    // const loader = new Loader([tiledMapResource]);
-    // game.start(loader).then(() => {
-    //   tiledMapResource.addTiledMapToScene(game.currentScene);
-    // })
+    const loader = new Loader([tiledMapResource]);
+    game.start(loader).then(() => {
+      tiledMapResource.addTiledMapToScene(game.currentScene);
+      game.add(player);
+      game.currentScene.camera.strategy.lockToActor(player)
+      game.currentScene.camera.zoom = 2
+    })
 
     return () => {
       game.stop()
